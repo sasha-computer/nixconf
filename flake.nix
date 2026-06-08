549c4abcf4407a8c9f2ba66c7c2c197868a110f7
Sasha <33594434+sasha-computer@users.noreply.github.com>
2026-06-08 14:37:57 +0100

    update flake.nix to use flake-parts (oh boi)

--- flake.nix
+++ flake.nix
@@ -3,12 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     helium = {
       url = "github:schembriaiden/helium-browser-nix-flake";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,24 +17,27 @@
 
   outputs =
     inputs@{
-      nixpkgs,
-      home-manager,
+      flake-parts,
       ...
     }:
-    {
-      nixosConfigurations.fw13 = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-            home-manager.users.sasha = import ./home.nix;
-            home-manager.backupFileExtension = "backup";
-          }
-        ];
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      flake = {
+        nixosConfigurations.fw13 = inputs.nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./configuration.nix
+            inputs.home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit inputs; };
+              home-manager.users.sasha = import ./home.nix;
+              home-manager.backupFileExtension = "backup";
+            }
+          ];
+        };
       };
     };
 }
